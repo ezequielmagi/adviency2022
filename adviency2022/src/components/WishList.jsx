f7ff0d4ad909c1ce6aa4c784f3f82492f56a4976
Ezequiel Magi <ezequielmaggi@gmail.com>
2022-12-07 16:58:14 -0300

    Añadiendo imagenes para postear

--- adviency2022/src/components/WishList.jsx
+++ adviency2022/src/components/WishList.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 export function Wishlist(){
 
-  const [ wishList , setWishList ] = useState( ['papas', 'fritas', 'cheddar', 'dasdsad'] )
+  const [ wishList , setWishList ] = useState( ['Mantecol', 'Anana Fizz', 'Pan dulce con muchas frutas'] )
   
   return(
     <div className="w-3/4 h-auto bg-slate-100 rounded-md p-2 mx-auto shadow-md shadow-red-700/40 md:w-2/4 md:my-2">
